70444d2cd78e464db685f291bb75c529c19b8519
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-03-16 19:15:12 +0000

    Update rule-113.py

--- 1-Dimensional/rule-113.py
+++ 1-Dimensional/rule-113.py
@@ -1,1 +1,41 @@
+from guizero import *
 
+size = 500
+
+root = App(width = (size * 3) + 20, title = "Cellular Automata By @SmashedFrenzy16")
+
+w = Waffle(root, height = size, width = size, dim = 3, pad = 0)
+
+y = 1
+
+
+def screen_update():
+
+
+    global y
+
+    if y < size:
+
+        for x in range(1, size - 1):
+
+            l = w.get_pixel(x - 1, y - 1)
+            m = w.get_pixel(x, y - 1)
+            r = w.get_pixel(x + 1, y - 1)
+
+            # Rule 113 of Cellular Automata
+
+            if (l == "red") and (m == "red") and (r == "white"):
+
+                w.set_pixel(x, y, "red")
+
+            if (l == "red") and (m == "white") and (r == "red"):
+
+                w.set_pixel(x, y, "red")
+
+            if (l == "red") and (m == "white") and (r == "white"):
+
+                w.set_pixel(x, y, "red")
+
+            if (l == "white") and (m == "white") and (r == "white"):
+
+                w.set_pixel(x, y, "red")
